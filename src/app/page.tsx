3e40f6cf8ddc428f20a4563fdb27a3e49b6fc1d5
PrathamSingh1 <prathamjii36@gmail.com>
2026-08-09 01:07:11 +0530

    added banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Banner } from "@/components/layout/banner";
 import Container from "@/components/layout/container";
 import { HorizontalLine } from "@/components/ui/horizontal-line";
 import { VerticalLine } from "@/components/ui/vertical-line";
@@ -5,10 +6,12 @@ import { VerticalLine } from "@/components/ui/vertical-line";
 export default function Home() {
   return (
     <Container className="w-full">
-      
       <VerticalLine className="left-[28%]" />
       <VerticalLine className="right-[28%]" />
       <HorizontalLine className="top-[22vh]" />
+      <div className="h-[22vh] overflow-hidden">
+        <Banner className="h-[22vh]" />
+      </div>
     </Container>
   );
 }
--- src/components/layout/banner.tsx
+++ src/components/layout/banner.tsx
@@ -1,13 +1,25 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-
-export const Banner = ({ className }: {
-  className?: string;
-}) => {
+export const Banner = ({ className }: { className?: string }) => {
   return (
-    <div className={cn("", className)}>
-      <Image src={"/banner/banner1.png"} />
+    <div
+      className={cn(
+        "absolute left-0 right-0 md:left-[28%] md:right-[28%] top-0 -z-0 pointer-events-auto dark:bg-black shadow-[0_4px_12px_rgba(2,6,23,0.04)] dark:shadow-[0_4px_12px_rgba(2,6,23,0.10)]",
+        className,
+      )}
+    >
+      <Image
+        src={"/banner/banner1.png"}
+        alt="banner image"
+        fill
+        fetchPriority="high"
+        quality={100}
+        className="object-cover object-center"
+      />
+      <div className="absolute inset-x-0 bottom-0 h-10 pointer-events-none z-5 bg-linear-to-t from-white/80 to-transparent dark:from-black/50 dark:to-transparent" />
+      <div className="absolute left-0 top-0 bottom-0 w-8 pointer-events-none z-20 bg-linear-to-r from-white/40 to-transparent dark:from-black/40 dark:to-transparent" />
+      <div className="absolute right-0 top-0 bottom-0 w-8 pointer-events-none z-20 bg-linear-to-l from-white/40 to-transparent dark:from-black/40 dark:to-transparent" />
     </div>
-  )
-}
+  );
+};
