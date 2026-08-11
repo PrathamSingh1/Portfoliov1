f2abca475da93f81300ca5c13320e1b70fc9e057
PrathamSingh1 <prathamjii36@gmail.com>
2026-08-11 16:47:48 +0530

    added more skill svg

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,9 +1,35 @@
 import { cn } from "@/lib/utils";
 import { HorizontalLine } from "../ui/horizontal-line";
 import { Heading } from "../ui/heading";
-import { Button } from "../ui/button";
 import { SkillButton } from "../ui/skill-button";
-import { ReactIcon } from "../icons/icons";
+import {
+  AWSIcon,
+  CloudflareIcon,
+  CSSIcon,
+  DockerIcon,
+  ExpressJsIcon,
+  FigmaIcon,
+  GitHubIcon2,
+  GitIcon,
+  GSAPIcon,
+  HTMLIcon,
+  JavaScriptIcon,
+  MongoDBIcon,
+  MotionIcon,
+  NextjsIcon,
+  NodeJsIcon,
+  PostgreSQLIcon,
+  PostmanIcon,
+  PrismaIcon,
+  ReactIcon,
+  ReactQueryIcon,
+  RedisIcon,
+  SQLIcon,
+  SVGIcon,
+  TailwindCSSIcon,
+  TypeScriptIcon,
+  VercelIcon,
+} from "../icons/icons";
 
 export const Skills = ({ className }: { className?: string }) => {
   return (
@@ -13,10 +39,114 @@ export const Skills = ({ className }: { className?: string }) => {
       <div className="mb-6">
         <HorizontalLine className="top-[40px]" />
       </div>
-      <div>
+      <div className="flex flex-wrap items-center gap-2">
+        <SkillButton>
+          <HTMLIcon />
+          HTML
+        </SkillButton>
+        <SkillButton>
+          <CSSIcon />
+          CSS
+        </SkillButton>
+        <SkillButton>
+          <JavaScriptIcon />
+          JavaScript
+        </SkillButton>
+        <SkillButton>
+          <TypeScriptIcon />
+          TypeScript
+        </SkillButton>
         <SkillButton>
           <ReactIcon />
           React
+        </SkillButton>
+        <SkillButton>
+          <TailwindCSSIcon />
+          TailwindCSS
+        </SkillButton>
+        <SkillButton>
+          <NextjsIcon />
+          NextJs
+        </SkillButton>
+        <SkillButton>
+          <MotionIcon />
+          Motion
+        </SkillButton>
+        <SkillButton>
+          <GSAPIcon />
+          GSAP
+        </SkillButton>
+        <SkillButton>
+          <ReactQueryIcon />
+          React Query
+        </SkillButton>
+        <SkillButton>
+          <NodeJsIcon />
+          NodeJs
+        </SkillButton>
+        <SkillButton>
+          <ExpressJsIcon />
+          ExpressJs
+        </SkillButton>
+        <SkillButton>
+          <PrismaIcon />
+          Prisma
+        </SkillButton>
+        <SkillButton>
+          <GitIcon />
+          Git
+        </SkillButton>
+        <SkillButton>
+          <GitHubIcon2 />
+          Github
+        </SkillButton>
+        <SkillButton>
+          <PostgreSQLIcon />
+          Postgres
+        </SkillButton>
+        <SkillButton>
+          <SQLIcon />
+          SQL
+        </SkillButton>
+        <SkillButton>
+          <MongoDBIcon />
+          MongoDB
+        </SkillButton>
+        <SkillButton>
+          <AWSIcon />
+          AWS
+        </SkillButton>
+        <SkillButton>
+          <CloudflareIcon />
+          CloudFlare
+        </SkillButton>
+        <SkillButton>
+          <FigmaIcon />
+          Figma
+        </SkillButton>
+        <SkillButton>
+          <SVGIcon />
+          SVG
+        </SkillButton>
+        <SkillButton>
+          <PostmanIcon />
+          Postman
+        </SkillButton>
+        <SkillButton>
+          <RedisIcon />
+          Redis
+        </SkillButton>
+        <SkillButton>
+          <DockerIcon />
+          Docker
+        </SkillButton>
+        <SkillButton>
+          <GitHubIcon2 />
+          Github Actions(CI/CD)
+        </SkillButton>
+        <SkillButton>
+          <VercelIcon />
+          Vercel
         </SkillButton>
       </div>
     </div>
--- src/components/ui/skill-button.tsx
+++ src/components/ui/skill-button.tsx
@@ -10,7 +10,7 @@ export const SkillButton = ({
   return (
     <button
       className={cn(
-        "inline-flex items-center justify-center gap-2 shadow-sm whitespace-nowrap rounded-md font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-3.5 [&_svg]:shrink-0 bg-card-color text-foreground hover:bg-card-color-foreground cursor-pointer py-2 h-6 px-2.5 text-xs hover:text-brand border dark:border-neutral-700/40 border-neutral-300/50",
+        "inline-flex items-center justify-center gap-1.5 shadow-sm whitespace-nowrap rounded-md font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-3 [&_svg]:shrink-0 bg-card-color text-foreground hover:bg-card-color-foreground py-2 h-[22px] px-1.5 text-xs hover:text-brand border dark:border-neutral-700/40 border-neutral-300/50 cursor-default",
         className,
       )}
     >
