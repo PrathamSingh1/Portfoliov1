833e1481eea7bf1b0e8d1f9bbe9e80d319dadb15
PrathamSingh1 <prathamjii36@gmail.com>
2026-08-18 21:03:34 +0530

    fixed blog card link

--- src/components/blog/blog-card.tsx
+++ src/components/blog/blog-card.tsx
@@ -19,42 +19,44 @@ export function BlogCard({ post }: BlogCardProps) {
 
   return (
     <Card className="group relative flex items-center justify-between h-full w-full">
-      <div>
-        <CardContent>
-          <div className="">
-            <Link href={`/blogs/${slug}`}>
-              <h3 className="transition-colors duration-200 text-sm font-figtree text-foreground">
-                {title}
-              </h3>
-            </Link>
-            <p className="text-sm font-figtree text-muted-foreground py-1 line-clamp-3">
-              {description}
-            </p>
-          </div>
-        </CardContent>
-        <CardFooter className="pt-0">
-          <div className="flex w-full flex-col space-y-3">
-            <div className="mt-1 flex items-center justify-between gap-2">
-              <time
-                className="text-muted-foreground font-figtree flex items-center gap-2 text-xs"
-                dateTime={date}
-              >
-                <IconCalendar size={14} />
-                {formattedDate}
-              </time>
+      <Link
+        href={`/blogs/${slug}`}
+        className="flex items-center justify-between w-full"
+      >
+        <div>
+          <CardContent>
+            <div className="">
+              <div>
+                <h3 className="transition-colors duration-200 text-sm font-figtree text-foreground">
+                  {title}
+                </h3>
+              </div>
+              <p className="text-sm font-figtree text-muted-foreground py-1 line-clamp-3">
+                {description}
+              </p>
             </div>
+          </CardContent>
+          <CardFooter className="pt-0">
+            <div className="flex w-full flex-col space-y-3">
+              <div className="mt-1 flex items-center justify-between gap-2">
+                <time
+                  className="text-muted-foreground font-figtree flex items-center gap-2 text-xs"
+                  dateTime={date}
+                >
+                  <IconCalendar size={14} />
+                  {formattedDate}
+                </time>
+              </div>
+            </div>
+          </CardFooter>
+        </div>
+        <div>
+          <div className="text-foreground/80 hover:text-foreground font-figtree text-sm flex items-center justify-center gap-2">
+            Read More
+            <IconArrowRight size={16} />
           </div>
-        </CardFooter>
-      </div>
-      <div>
-        <Link
-          href={`/blogs/${slug}`}
-          className="text-foreground/80 hover:text-foreground font-figtree text-sm flex items-center justify-center gap-2"
-        >
-          Read More
-          <IconArrowRight size={16} />
-        </Link>
-      </div>
+        </div>
+      </Link>
       <div className="absolute inset-0 top-6 bottom-6 -mx-3 -my-3 rounded-xl dark:bg-muted bg-muted-foreground/30 opacity-0 duration-300 group-hover:opacity-20 pointer-events-none"></div>
     </Card>
   );
